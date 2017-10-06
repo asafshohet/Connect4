db1860d4b042ba561f7771bc704c0890c37b0b2c
unknown <asafshohet@gmail.com>
2017-10-06 13:30:48 +0300

    1 move is correct

--- connect4.pl
+++ connect4.pl
@@ -27,25 +27,26 @@ board_full([]).
 board_full([Col|Columns]):-
 	column_full([Col],1), board_full(Columns).
 
+%TODO
 valid_move(Board, ColNum):-
-	integer(ColNum), ColNum =< 7, ColNum>0, \+ column_full(Board, ColNum).
+	member(ColNum,[1,2,3,4,5,6,7]), \+ column_full(Board, ColNum).
 
 %%%%%%%%%%%%%%%%%%%%%%% update board %%%%%%%%%%%%
 
 update_board([],[],_,_,_).
 
 update_board([Col|Columns], NewBoard, Turn, ColNumToUpdate, ColNumToUpdate):-
-	NextColumn is ColNumToUpdate +1,
+	!,NextColumn is ColNumToUpdate +1,
 	update_board(Columns, TempBoard, Turn, ColNumToUpdate, NextColumn),
-	append(Col, [Turn], NewCol), 
+	append(Col, [Turn], NewCol),
 	append([NewCol],TempBoard, NewBoard).
 
 update_board([Col|Columns], NewBoard, Turn, ColNumToUpdate, CurrentColoumn):-
-	NextColumn is CurrentColoumn +1,
+	!,NextColumn is CurrentColoumn +1,
 	update_board(Columns, TempBoard, Turn, ColNumToUpdate, NextColumn),
 	append([Col],TempBoard, NewBoard).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%% goal %%%%%%%%%%%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%% goal %%%%%%%%%%%%
 
 is_color(_, 0,_):-!,false.
 is_color([],_,_):-!,false.
@@ -65,84 +66,84 @@ is_color([_|Columns], ColNum, Index, Turn):-
 	NewNum is ColNum-1, is_color(Columns, NewNum, Index, Turn).
 
 turn_diagonal_up_left(Board, ColNum, Index, Turn, Count):-
-	NewColumn is ColNum-1, NewIndex is Index-1,  
+	NewColumn is ColNum-1, NewIndex is Index-1,
 	(
-		is_color(Board, NewColumn, NewIndex, Turn), !, 
+		is_color(Board, NewColumn, NewIndex, Turn), !,
 		turn_diagonal_up_left(Board, NewColumn, NewIndex, Turn, NewCount),
 		Count is NewCount+1;
-		
+
 		Count is 0
 	).
 
 turn_diagonal_up_right(Board, ColNum, Index, Turn, Count):-
-	NewColumn is ColNum+1, NewIndex is Index+1,  
+	NewColumn is ColNum+1, NewIndex is Index+1,
 	(
-		is_color(Board, NewColumn, NewIndex, Turn), !, 
+		is_color(Board, NewColumn, NewIndex, Turn), !,
 		turn_diagonal_up_right(Board, NewColumn, NewIndex, Turn, NewCount),
 		Count is NewCount+1;
-		
+
 		Count is 0
 	).
 turn_diagonal_down_left(Board, ColNum, Index, Turn, Count):-
-	NewColumn is ColNum-1, NewIndex is Index+1,  
+	NewColumn is ColNum-1, NewIndex is Index+1,
 	(
-		is_color(Board, NewColumn, NewIndex, Turn), !, 
+		is_color(Board, NewColumn, NewIndex, Turn), !,
 		turn_diagonal_down_left(Board, NewColumn, NewIndex, Turn, NewCount),
 		Count is NewCount+1;
-		
+
 		Count is 0
 	).
 turn_diagonal_down_right(Board, ColNum, Index, Turn, Count):-
-	NewColumn is ColNum+1, NewIndex is Index-1,  
+	NewColumn is ColNum+1, NewIndex is Index-1,
 	(
-		is_color(Board, NewColumn, NewIndex, Turn), !, 
+		is_color(Board, NewColumn, NewIndex, Turn), !,
 		turn_diagonal_down_right(Board, NewColumn, NewIndex, Turn, NewCount),
 		Count is NewCount+1;
-		
+
 		Count is 0
 	).
 
 turn_row_right(Board, ColNum, Index, Turn, Count):-
-	NewColumn is ColNum+1,  
+	NewColumn is ColNum+1,
 	(
-		is_color(Board, NewColumn, Index, Turn), !, 
+		is_color(Board, NewColumn, Index, Turn), !,
 		turn_row_right(Board, NewColumn, Index, Turn, NewCount),
 		Count is NewCount+1;
-		
+
 		Count is 0
 	).
 turn_row_left(Board, ColNum, Index, Turn, Count):-
-	NewColumn is ColNum-1,  
+	NewColumn is ColNum-1,
 	(
-		is_color(Board, NewColumn, Index, Turn), !, 
+		is_color(Board, NewColumn, Index, Turn), !,
 		turn_row_left(Board, NewColumn, Index, Turn, NewCount),
 		Count is NewCount+1;
-		
+
 		Count is 0
 	).
 
 turn_col_down(Board, ColNum, Index, Turn, Count):-
 	NewIndex is Index-1,
 	(
-		is_color(Board, ColNum, NewIndex, Turn), !, 
+		is_color(Board, ColNum, NewIndex, Turn), !,
 		turn_col_down(Board, ColNum, NewIndex, Turn, NewCount),
 		Count is NewCount+1;
-		
+
 		Count is 0
 	).
-	
+
 goal(Board,ColNum, Index, Turn, AmountNeeded):-
 	turn_col_down(Board, ColNum, Index, Turn, DownTurnCount), DownTurnCount >= AmountNeeded, !.
-goal(Board,ColNum, Index, Turn, AmountNeeded):-	
-	turn_row_left(Board, ColNum, Index, Turn, LeftTurnCount), 
+goal(Board,ColNum, Index, Turn, AmountNeeded):-
+	turn_row_left(Board, ColNum, Index, Turn, LeftTurnCount),
 	turn_row_right(Board, ColNum, Index, Turn, RightTurnCount), LeftTurnCount+RightTurnCount >= AmountNeeded, !.
-goal(Board,ColNum, Index, Turn, AmountNeeded):-	
-	turn_diagonal_up_left(Board, ColNum, Index, Turn, LeftTurnCount), 
+goal(Board,ColNum, Index, Turn, AmountNeeded):-
+	turn_diagonal_up_left(Board, ColNum, Index, Turn, LeftTurnCount),
 	turn_diagonal_up_right(Board, ColNum, Index, Turn, RightTurnCount), LeftTurnCount+RightTurnCount >= AmountNeeded, !.
-goal(Board,ColNum, Index, Turn, AmountNeeded):-	
-	turn_diagonal_down_left(Board, ColNum, Index, Turn, LeftTurnCount), 
+goal(Board,ColNum, Index, Turn, AmountNeeded):-
+	turn_diagonal_down_left(Board, ColNum, Index, Turn, LeftTurnCount),
 	turn_diagonal_down_right(Board, ColNum, Index, Turn, RightTurnCount), LeftTurnCount+RightTurnCount >= AmountNeeded, !.
-	
+
 goal(Board,ColNum, Turn):-
 	coloumn_len(Board, ColNum, Index), goal(Board, ColNum, Index, Turn, 3).
 
@@ -171,7 +172,7 @@ print_column_numbers([_|Columns], Count):-
 print_board(Board):-
 	print_column_numbers(Board,1),nl,nl,
 	print_board(Board, 6),nl.
-	
+
 
 %%%%%%%%%%%%%% entry point %%%%%%%%%%%%
 
@@ -179,10 +180,10 @@ switch_turn(x, o).
 switch_turn(o, x).
 
 update_and_play(Board, Turn, ColNum):-
-	update_board(Board, NewBoard, Turn, ColNum, 1),
+	update_board(Board, NewBoard, Turn, ColNum, 1),print_board(NewBoard),nl,
 	(
-		goal(NewBoard, ColNum, Turn),!, print_board(NewBoard), nl, write(Turn), write(' has prevaled! Game Over!'), nl;
-		switch_turn(Turn,NewTurn), print_board(NewBoard), play(NewBoard,NewTurn)
+		goal(NewBoard, ColNum, Turn),!, write(Turn), write(' has prevaled! Game Over!'), nl;
+		switch_turn(Turn,NewTurn), play(NewBoard,NewTurn)
 	).
 
 play(Board, _):-
@@ -190,22 +191,22 @@ play(Board, _):-
 
 %user plays
 play(Board, o):-
-	write(Turn), write(': select column (1-7)'),nl,read(ColNum), 
+	write('o: select column (1-7)'),nl,read(ColNum),
 	(
-		valid_move(Board, ColNum), !, update_and_play(Board, Turn, ColNum);
-		write('column '), write(ColNum), write(' is full or invalid. Please select another one!'), nl, nl, play(Board,Turn)
+		integer(ColNum), !, valid_move(Board, ColNum), update_and_play(Board, o, ColNum);
+		write('column '), write(ColNum), write(' is full or invalid. Please select another one!'), nl, nl, play(Board,o)
 	).
 
 % computer plays
 play(Board, x):-
-	alphabeta(o-_-Board,-1000,1000,Turn/ColNum/_,_,1), 
-	update_and_play(Board, Turn, ColNum).
+	alphabeta(o-_-Board,-1000,1000,_-ColNum-_,_,1),
+	update_and_play(Board, x, ColNum).
 
-play:- init_board(Board),print_board(Board), play(Board, x).
+play:- init_board(Board),print_board(Board), !, play(Board, x).
 	%get_difficulty(Difficulty),asserta(dif(Difficulty)),
 
 
-	
+
 %%%%%%%%%%%%%% alpha-beta %%%%%%%%%%%%
 % Pos is Turn-ColNum-NewBoard. Turn is the Turn played. ColNum is the column played. NewBoard is the board created this play
 :- dynamic
@@ -222,7 +223,7 @@ save_if_win(Turn-ColNum-NewBoard, Depth, DidWeJustWin):-
 	true.
 
 delete_if_win(Turn-_-_, DidWeJustWin):-
-	nonvar(DidWeJustWin), !, retractall(win(Turn,_)); 
+	nonvar(DidWeJustWin), !, retractall(win(Turn,_));
 	true.
 
 alphabeta(Pos,Alpha,Beta,GoodPos,Val,Depth):-
@@ -230,13 +231,12 @@ alphabeta(Pos,Alpha,Beta,GoodPos,Val,Depth):-
 	boundedbest(PosList,Alpha,Beta,GoodPos,Val,Depth);
 	staticval(Pos,Val).
 
+%save_if_win(Pos,Depth,DidWeJustWin), %My addition
+%delete_if_win(Pos,DidWeJustWin).%My addition
 boundedbest([Pos|PosList],Alpha,Beta,GoodPos,GoodVal,Depth):-
 	NewDepth is Depth - 1,
-	save_if_win(Pos,Depth,DidWeJustWin), %My addition
 	alphabeta(Pos, Alpha,Beta,_, Val,NewDepth),
-	goodenough(PosList,Alpha,Beta,Pos,Val,GoodPos,GoodVal,Depth),
-	delete_if_win(Pos,DidWeJustWin).%My addition
-				
+	goodenough(PosList,Alpha,Beta,Pos,Val,GoodPos,GoodVal,Depth).
 
 goodenough([],_,_,Pos,Val,Pos,Val,_):- !.     % No other candidate
 
@@ -262,9 +262,9 @@ betterof(Pos,Val,_,Val1,Pos,Val):-         % Pos better then Pos1
 	max_to_move(Pos), Val < Val1,!.
 
 betterof(_,_,Pos1,Val1,Pos1,Val1).             % Otherwise Pos1 better
-	
 
-	
+
+
 % list of all possible next states. template is Turn/ColNum/NewBoard
 moves(Turn-_-Board, ListOfMoves):-
 	switch_turn(Turn,NextTurn),  %switch the player
@@ -272,25 +272,32 @@ moves(Turn-_-Board, ListOfMoves):-
 
 % heuristic functions %
 amount_strait(Board, ColNum, Index, Turn, Val):-
-	goal(Board, ColNum, Index, Turn, 3), !, Val is 4; 
-	goal(Board, ColNum, Index, Turn, 2), !, Val is 3; 
+	%goal(Board, ColNum, Index, Turn, 3), !, Val is 4;
+	%goal(Board, ColNum, Index, Turn, 2), !, Val is 3;
 	goal(Board, ColNum, Index, Turn, 1), !, Val is 2;
-	Val is 1.
+	Val is 0.
 
+
+val_sign(Turn,Val,NewVal):-
+	min_to_move(Turn-_-_), !, NewVal is Val;
+	NewVal is -1*Val.
 
 staticval(Turn-ColNum-Board, Val):-
-	switch_turn(Turn,OtherPlayer),
+	coloumn_len(Board, ColNum, Index),	amount_strait(Board, ColNum, Index, Turn, Res), val_sign(Turn,Res,Val).
+	/*
+	switch_turn(Turn,_),
 	(
 		% if other player won - give it the most negative value
-		win(OtherPlayer,WonDepth),!, Val is -7*WonDepth;
-		
+		%win(OtherPlayer,WonDepth),!, Res is -7*WonDepth,val_sign(Turn,Res,Val);
+
 		%if we won - give it high value (less than absolute value of 'OtherPlayer' winning
-		win(Turn,WonDepth), !, Val is 4*WonDepth;
-		
+		%win(Turn,WonDepth), !, Res is 4*WonDepth, val_sign(Turn,Res,Val);
+
 		% no one won so far - create static evaluation of new move
-		coloumn_len(Board, ColNum, Index),	amount_strait(Board, ColNum, Index, Turn, Val)
+
 	).
 
+*/
 
 
 
